9bbc5fffce1aa0be010a8e54e020a72ff7370ff1
tykim83 <agate.fabrizio83@gmail.com>
2025-02-24 17:04:32 +0000

    Working basic project

---
+++ FabRaylib/FabRaylibTemplate/Application.cs
@@ -0,0 +1,32 @@
+﻿using Raylib_cs;
+using System.Runtime.InteropServices.JavaScript;
+
+namespace FabRaylibTemplate;
+
+public partial class Application
+{
+    private static Texture2D logo;
+
+    public static void Main()
+    {
+        Raylib.InitWindow(512, 512, "FabRaylibTemplate");
+        Raylib.SetTargetFPS(60);
+
+        logo = Raylib.LoadTexture("Resources/raylib_logo.png");
+    }
+
+    [JSExport]
+    public static void UpdateFrame()
+    {
+        Raylib.BeginDrawing();
+
+        Raylib.ClearBackground(Color.White);
+
+        Raylib.DrawFPS(4, 4);
+        Raylib.DrawText("All systems operational!", 4, 32, 20, Color.Maroon);
+
+        Raylib.DrawTexture(logo, 4, 64, Color.White);
+
+        Raylib.EndDrawing();
+    }
+}
